06c7dca0875e13d6d5484c0f7786befca9d3fd23
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2024-05-31 15:46:05 +0500

    change forgot password handler created

---
+++ api/handlers/auth/confirmforgotpw.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/gin-gonic/gin"
+	"github.com/goledgerdev/goprocess-api/utils"
+	"github.com/google/logger"
+)
+
+type confirmForgotPasswordForm struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	OTP      string `json:"otp" binding:"required"`
+}
+
+func (a *Auth) ConfirmForgotPassword(c *gin.Context) {
+	var form confirmForgotPasswordForm
+	if err := c.Bind(&form); err != nil {
+		logger.Error(err)
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	username := form.Username
+	password := form.Password
+	otp := form.OTP
+	clientID := os.Getenv("COGNITO_APP_CLIENT_ID")
+	clientSecret := os.Getenv("COGNITO_APP_CLIENT_SECRET")
+
+	input := cognito.ConfirmForgotPasswordInput{
+		ClientId:         aws.String(clientID),
+		Username:         aws.String(username),
+		Password:         aws.String(password),
+		SecretHash:       aws.String(utils.ComputeSecretHash(clientSecret, username, clientID)),
+		ConfirmationCode: aws.String(otp),
+	}
+
+	req, output := a.CognitoClient.ConfirmForgotPasswordRequest(&input)
+
+	err := req.Send()
+	if err != nil {
+		logger.Error(err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, output.String())
+}
