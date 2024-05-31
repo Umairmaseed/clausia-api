9774eddc59c48b8bbaed7e58bc14de64c3f1d216
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2024-05-31 15:47:48 +0500

    resend otp handler created

---
+++ api/handlers/auth/resend.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/gin-gonic/gin"
+	"github.com/goledgerdev/goprocess-api/utils"
+	"github.com/google/logger"
+)
+
+type resendCodeForm struct {
+	Username string `json:"username" binding:"required"`
+}
+
+func (a *Auth) ResendCode(c *gin.Context) {
+	var form resendCodeForm
+	var err error
+
+	if err := c.Bind(&form); err != nil {
+		logger.Error(err)
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	username := form.Username
+
+	var secretHash string
+	if a.AppClientSecret != "" {
+		secretHash = utils.ComputeSecretHash(a.AppClientSecret, username, a.AppClientID)
+	}
+
+	params := &cognito.ResendConfirmationCodeInput{
+		SecretHash: &secretHash,
+		Username:   aws.String(username),
+		ClientId:   aws.String(a.AppClientID),
+	}
+
+	req, out := a.CognitoClient.ResendConfirmationCodeRequest(params)
+
+	err = req.Send()
+	if err != nil {
+		logger.Error(err)
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, out.String())
+}
